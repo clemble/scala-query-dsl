93415ba6278e0e825b8cd90e60fe506ab7652d25
Anton Oparin <mavarazy@gmail.com>
2016-09-12 08:48:23 +0300

    More expressions and more tests

--- query/src/main/scala/com/clemble/query/string/ExpressionParser.scala
+++ query/src/main/scala/com/clemble/query/string/ExpressionParser.scala
@@ -68,6 +68,36 @@ case object EqualsExpressionParser extends ExpressionParser {
 }
 
 /**
+  *  Greater then queries
+  *  Similar to less then, all value will be treated as numbers
+  *
+  *    Example:
+  *      ?price-gt=100
+  *    will be translated as GreaterThen("price", 100)
+  *
+  *    If multiple query parameters were specified, the highest value will be taken
+  *
+  *    Example:
+  *      ?price-gt=100&price-gt=190
+  *    will be translated as GreaterThen("price", 190)
+  */
+case class GreaterThenExpressionParser(gtParam: String = "-gt") extends ExpressionParser {
+
+  override def isDefinedAt(x: (String, Seq[String])) = x._1.endsWith(gtParam)
+
+  override def apply(x: (String, Seq[String])): Expression = {
+    val (key, values) = x
+    if (!key.endsWith(gtParam))
+      throw new IllegalArgumentException(s"Ending must be $gtParam")
+    if (values.isEmpty)
+      return Empty
+    val maxValue = values.map(BigDecimal(_)).max
+    GreaterThen(key.substring(0, key.length - gtParam.length), maxValue)
+  }
+
+}
+
+/**
   *  Greater then equals queries
   *  Similar to less then, all value will be treated as numbers
   *
@@ -99,6 +129,36 @@ case class GreaterThenEqualsExpressionParser(gteParam: String = "-gte") extends 
 
 /**
   *   Less then queries
+  *   All values will be treated as numbers
+  *
+  *   Example:
+  *    ?price-lt=100
+  *   will be translated as LessThen("price", 100)
+  *
+  *   If multiple query parameters were specified, the lowest value will be taken
+  *
+  *   Example:
+  *    ?price-lt=100&price-lt=50
+  *   will be translated as LessThen("price", 50)
+  */
+case class LessThenExpressionParser(ltParam: String = "-lt") extends ExpressionParser {
+
+  override def isDefinedAt(x: (String, Seq[String])) = x._1.endsWith(ltParam)
+
+  override def apply(x : (String, Seq[String])): Expression = {
+    val (key, values) = x
+    if (!key.endsWith(ltParam))
+      throw new IllegalArgumentException(s"Ending must be $ltParam")
+    if (values.isEmpty)
+      return Empty
+    val maxValue = values.map(BigDecimal(_)).min
+    LessThen(key.substring(0, key.length - ltParam.length), maxValue)
+  }
+
+}
+
+/**
+  *   Less then equals queries
   *   All values will be treated as numbers
   *
   *   Example:
